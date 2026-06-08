3576bf37a80d2809fa03b56f6c4e72dd0507cffb
jgarciaanria-dot <jgarcia.anria@gmail.com>
2026-06-08 13:46:52 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
   sheets: {
-    scriptUrl: 'https://script.google.com/macros/s/AKfycbzY_gv8tb5Z_ENdkgnGJUjnPBAal4kaXhBOnhN1JUUqTsJecasW3t6FNhosJ-Ny2ZoK/exec',
+    scriptUrl: 'https://script.google.com/macros/s/AKfycbyX7R9SwuwCmn_bX3cwH2jCcYAr67QYt4UKkuxoK4WfSm8m0SGHxXmnUVWAZgPpowCy/exec',
     sheetId: '1p8NxajPAoxiaQnsomWNo-8P8aL_FwNjBCWT8YXz1G7c'
   },
   negocio: {
